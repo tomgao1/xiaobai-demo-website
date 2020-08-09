b93e2835e3cad03c2a13838a595f75d6cc7671c4
tomgao <406038564@qq.com>
2020-08-09 15:30:28 +0800

    deploy

---
+++ precache-manifest.8268e5719df3bf8726f226d82d5d142b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb05e6861c82cd0dc4b0",
+    "url": "/xiaobai-demo-website/css/app.a7657ad1.css"
+  },
+  {
+    "revision": "b93c487ae8b2612a4fd0d3848745fa2d",
+    "url": "/xiaobai-demo-website/index.html"
+  },
+  {
+    "revision": "eb05e6861c82cd0dc4b0",
+    "url": "/xiaobai-demo-website/js/app.61acf662.js"
+  },
+  {
+    "revision": "72783208fcbe14d38b0d",
+    "url": "/xiaobai-demo-website/js/chunk-vendors.81907bf4.js"
+  },
+  {
+    "revision": "35241d2c8512e1d09dde6f5da90ad971",
+    "url": "/xiaobai-demo-website/manifest.json"
+  },
+  {
+    "revision": "0669baa92f7037c2c9981ba11afc17d1",
+    "url": "/xiaobai-demo-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiaobai-demo-website/robots.txt"
+  }
+]);
